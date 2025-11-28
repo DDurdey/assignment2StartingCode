2939a91ff725326d52f446cfd7ef06fe7bdf7620
Edwin Wong <edwin.wong@edu.sait.ca>
2025-11-27 23:24:59 -0700

    Update MyStack.java

--- src/implementations/MyStack.java
+++ src/implementations/MyStack.java
@@ -5,5 +5,35 @@ import utilities.Iterator;
 import java.util.EmptyStackException;
 
 public class MyStack<E> implements StackADT<E> {
-  private MyArrayList<E> list;
+    private MyArrayList<E> list;
+
+    public MyStack() {
+        list = new MyArrayList<E>();
+    }
+
+    @Override
+    public void push(E toAdd) throws NullPointerException {
+        if (toAdd == null) {
+            throw new NullPointerException("Cannot push null elements to the stack");
+        }
+        list.add(toAdd);
+    }
+
+    @Override
+    public E pop() throws EmptyStackException {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return list.remove(list.size() - 1);
+    }
+
+    @Override
+    public E peek() throws EmptyStackException {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return list.get(list.size() - 1);
+    }
+
+    
 }
