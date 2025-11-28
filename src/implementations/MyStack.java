cb1badabfc370b6e251be76082a884ca2637740f
Edwin Wong <edwin.wong@edu.sait.ca>
2025-11-27 23:23:09 -0700

    Im a dumbass and deleted my shit its back now

--- src/implementations/MyStack.java
+++ src/implementations/MyStack.java
@@ -3,3 +3,7 @@ package implementations;
 import utilities.StackADT;
 import utilities.Iterator;
 import java.util.EmptyStackException;
+
+public class MyStack<E> implements StackADT<E> {
+  private MyArrayList<E> list;
+}
